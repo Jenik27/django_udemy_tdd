dc7a53ca0d9e2560c4d04a286045918871f177ef
Jenik27 <jtomicek7@gmail.com>
2024-06-12 16:08:14 +0200

    code not working

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ba#==ej6xs&$!vt43lw+mvrj)qga+!wx)vhr(6*du&f*+-0zd+'
+SECRET_KEY = os.getenv('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.getenv('ALLOWED_HOSTS', '').split(',')
+    )
+)
 
 
 # Application definition
